e42b962e797ef4dd870a995ebf3ca2afebb8de09
shkyb <shakib.alipour@gmail.com>
2025-04-06 00:51:27 +0200

    created navItem component and added to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
-import { HiBars3, HiXMark } from "react-icons/hi2";
+import NavItem from "./NavItem";
+import { HiBars3, HiXMark, HiMiniArrowUpRight, HiOutlineBriefcase } from "react-icons/hi2";
+import { RiBehanceFill, RiLinkedinFill, RiGithubFill } from "react-icons/ri";
+
 
 
 
@@ -7,18 +10,28 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="w-full px-6 py-4 bg-white shadow-md fixed top-0 left-0 z-50">
-      <div className="max-w-6xl mx-auto flex items-center justify-between">
+    <nav className="w-full  py-3 bg-stone-50 border-b border-stone-200 fixed top-0 left-0 z-50">
+      <div className="max-w-screen-2xl px-6 md:px-12 mx-auto flex items-center justify-between">
         {/* Logo */}
-        <div className="text-xl font-bold text-gray-900">
-          shakib.design
+        <div className="text-normal uppercase text-stone-900  hover:text-blue-600">
+            <a href="./" className="inline-flex gap-2">
+              <div className="h-6 w-6 p-0.5">{<HiOutlineBriefcase size={20}/>}
+              </div>Shakib Alipour
+            </a>
         </div>
 
         {/* Desktop Menu */}
-        <ul className="hidden md:flex space-x-6 text-gray-700 font-medium">
-          <li><a href="#projects" className="hover:text-blue-600">Projects</a></li>
-          <li><a href="#about" className="hover:text-blue-600">About</a></li>
-          <li><a href="#contact" className="hover:text-blue-600">Contact</a></li>
+        <ul className="hidden md:flex space-x-6 ">
+          <NavItem text="Projects" count={6} href="#projects" />
+          <NavItem text="About" href="#about" />
+          <NavItem text="Résumé" icon={<HiMiniArrowUpRight />} href="#resume" />
+        </ul>
+
+        {/* Socials */}
+        <ul  className="hidden md:flex space-x-6">
+            <NavItem icon={<RiGithubFill size={20} />} href="https://github.com/shkyb" />                     
+            <NavItem icon={<RiBehanceFill size={20} />} href="https://behance.net/shakib-alipour" />
+            <NavItem icon={<RiLinkedinFill size={20} />} href="https://linkedin.com/in/shakib-alipour" />
         </ul>
 
         {/* Mobile Hamburger */}
@@ -33,7 +46,7 @@ export default function Navbar() {
 
       {/* Mobile Dropdown */}
       {isOpen && (
-        <ul className="md:hidden mt-4 space-y-4 text-gray-700 font-medium px-4">
+        <ul className="md:hidden mt-4 space-y-4 text-stone-900 font-medium px-4">
           <li><a href="#projects" onClick={() => setIsOpen(false)}>Projects</a></li>
           <li><a href="#about" onClick={() => setIsOpen(false)}>About</a></li>
           <li><a href="#contact" onClick={() => setIsOpen(false)}>Contact</a></li>
---
+++ src/components/navItem.jsx
@@ -0,0 +1,18 @@
+export default function NavItem({ text = null, count = null, icon = null, href = "#" }) {
+    return (
+      <li>
+        <a
+          href={href}
+          target="_blank"
+          className="flex items-center uppercase gap-1 text-stone-900 hover:text-red-500 font-normal transition-color delay-150"
+        >      
+          <span>{text}</span>
+          {count !== null && (
+            <sup className="text-normal uppercase text-stone-900">{count}</sup>
+          )}
+          {icon && <span>{icon}</span>}
+        </a>
+      </li>
+    );
+  }
+  
