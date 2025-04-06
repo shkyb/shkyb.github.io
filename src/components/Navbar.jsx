71d5523c726f22a74ca698fa7c3c65b6e709a88e
shkyb <shakib.alipour@gmail.com>
2025-04-06 23:39:30 +0200

    navbar version 1.0 is complete and ready to launch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="w-full  py-3 bg-stone-50 border-b border-stone-200 fixed top-0 left-0 z-50">
+    <nav className="w-full py-3 bg-stone-50 border-b border-stone-200 fixed top-0 left-0 z-999">
       <div className="max-w-screen-2xl px-6 md:px-12 mx-auto flex items-center justify-between">
         {/* Logo */}
         <div className="md:w-4/12"> <NavLogo text="Shakib Alipour" icon={<HiOutlineBriefcase size={20} />} />
@@ -25,10 +25,10 @@ export default function Navbar() {
         </ul>
 
         {/* Socials */}
-        <ul  className="hidden md:flex w-4/12 justify-end space-x-3">
-            <NavItem icon={<RiGithubFill size={20} />} href="https://github.com/shkyb" />                     
-            <NavItem icon={<RiBehanceFill size={20} />} href="https://behance.net/shakib-alipour" />
-            <NavItem icon={<RiLinkedinFill size={20} />} href="https://linkedin.com/in/shakib-alipour" />
+        <ul className="hidden md:flex w-4/12 justify-end space-x-3">
+          <NavItem icon={<RiGithubFill size={20} />} href="https://github.com/shkyb" />
+          <NavItem icon={<RiBehanceFill size={20} />} href="https://behance.net/shakib-alipour" />
+          <NavItem icon={<RiLinkedinFill size={20} />} href="https://linkedin.com/in/shakib-alipour" />
         </ul>
 
         {/* Mobile Hamburger */}
@@ -43,11 +43,23 @@ export default function Navbar() {
 
       {/* Mobile Dropdown */}
       {isOpen && (
-        <ul className="md:hidden mt-4 space-y-4 text-stone-900 font-medium px-4">
-            <NavItem icon={<RiGithubFill size={20} />} href="https://github.com/shkyb" />                     
-            <NavItem icon={<RiBehanceFill size={20} />} href="https://behance.net/shakib-alipour" />
-            <NavItem icon={<RiLinkedinFill size={20} />} href="https://linkedin.com/in/shakib-alipour" />
-        </ul>
+        <div className="md:hidden bg-stone-50 flex flex-col w-full justify-between pt-24 pb-14 h-screen">
+          <div>
+            <ul className="space-y-8 text-2xl px-6">
+              <NavItem text="Projects" count={6} href="./projects" />
+              <NavItem text="About" href="./about" />
+              <NavItem text="Résumé" icon={<HiMiniArrowUpRight />} href="#resume" />
+            </ul>
+          </div>
+          <div className="w-full px-6 border-t align-middle border-stone-200">
+            <ul className="align-middle mt-4 flex gap-8">
+              <NavItem icon={<RiGithubFill size={24} />} href="https://github.com/shkyb" />
+              <NavItem icon={<RiBehanceFill size={24} />} href="https://behance.net/shakib-alipour" />
+              <NavItem icon={<RiLinkedinFill size={24} />} href="https://linkedin.com/in/shakib-alipour" />
+            </ul>
+          </div>
+        </div>
+
       )}
     </nav>
   );
