8f53a0a8d06e38b6c1fd3de6ad25a003a5bc5cee
shkyb <shakib.alipour@gmail.com>
2025-04-16 23:54:56 +0200

    modified tag component

--- src/components/tag.jsx
+++ src/components/tag.jsx
@@ -1,6 +1,6 @@
 export default function Tag({ label }) {
     return (
-      <span className="bg-stone-100 border border-stone-300 text-xs px-3 py-1 rounded-full">
+      <span className="bg-stone-200 text-xs font-medium px-3 py-1 items-center justify-center ">
         {label}
       </span>
     );
