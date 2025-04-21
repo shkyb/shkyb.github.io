b5ec163dbf929c729e9e2fdeccdd574898f015ff
shkyb <shakib.alipour@gmail.com>
2025-04-21 12:48:54 +0200

    add onClick to cta

--- src/components/cta.jsx
+++ src/components/cta.jsx
@@ -1,13 +1,13 @@
-export default function CTA({ text, link, icon = null , target = "_blank"}) {
-    return (
-      <a
-        href={link}
-        target={target}
-        className="inline-flex justify-center items-center gap-1 w-fit mt-4 px-12 py-4 rounded-full outline-1 outline-stone-900 bg-stone-50 text-stone-900 text-base font-normal uppercase leading-normal hover:bg-stone-900 hover:text-stone-50 transition-colors"
-      >
-        {text}
-        {icon}
-      </a>
-    );
-  }
-  
+export default function CTA({ text, link, icon = null, target = "_blank", onClick }) {
+  return (
+    <a
+      href={link}
+      target={target}
+      onClick={onClick}
+      className="inline-flex justify-center items-center gap-1 w-fit mt-4 px-12 py-4 rounded-full outline-1 outline-stone-900 bg-stone-50 text-stone-900 text-base font-normal uppercase leading-normal hover:bg-stone-900 hover:text-stone-50 transition-colors"
+    >
+      {text}
+      {icon}
+    </a>
+  );
+}
