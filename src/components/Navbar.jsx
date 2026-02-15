922229d0626abf292c2ff11db513e3267d26bd51
shkyb <shakib.alipour@gmail.com>
2026-02-15 14:33:26 +0100

    Navbar: projects & about disabled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,8 +19,8 @@ export default function Navbar() {
 
         {/* Desktop Menu */}
         <ul className="hidden md:flex w-4/12 justify-between ">
-          <NavItem text="Projects" count={6} href="#projects" />
-          <NavItem text="About" href="#about" />
+          {/* <NavItem text="Projects" count={6} href="#projects" /> */}
+          {/* <NavItem text="About" href="#about" /> */}
           <NavItem text="Résumé" icon={<HiMiniArrowUpRight />} href="https://docs.google.com/document/d/1o0fNXL66ROGPvPpEJDpwa3XDLUZQflqOfrZEOSSTsmI/edit?usp=sharing" />
         </ul>
 
@@ -46,8 +46,8 @@ export default function Navbar() {
         <div className="md:hidden bg-stone-50 flex flex-col w-full justify-between pt-24 pb-14 h-screen">
           <div>
             <ul className="space-y-8 text-2xl px-6">
-              <NavItem text="Projects" count={6} href="./projects" />
-              <NavItem text="About" href="./about" />
+              {/* <NavItem text="Projects" count={6} href="./projects" /> */}
+              {/* <NavItem text="About" href="./about" /> */}
               <NavItem text="Résumé" icon={<HiMiniArrowUpRight />} href="https://docs.google.com/document/d/1o0fNXL66ROGPvPpEJDpwa3XDLUZQflqOfrZEOSSTsmI/edit?usp=sharing" />
             </ul>
           </div>
