cd344b721cbcdb3bbb833c8e4155b7ceea3b9243
shkyb <shakib.alipour@gmail.com>
2026-02-21 15:23:53 +0100

    shadcn installed

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+import path from "path"
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  base: '/',
+  plugins: [react(), tailwindcss()],
+  base: "/", // this is for the custom domain (shakib.design)
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
