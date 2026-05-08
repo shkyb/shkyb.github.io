e7f5f1ebee14d6858e7dbf71002a9c597846d49a
shkyb <shakib.alipour@gmail.com>
2026-05-08 13:23:38 +0200

    ita added to routing

--- src/case-studies/registry.js
+++ src/case-studies/registry.js
@@ -1,9 +1,11 @@
 import { dHeartCase } from "./d-heart"
 import { demoCase } from "./demo"
 import { nathanCase } from "./nathan"
+import { itaAirwaysCase } from "./ita-airways"
 
 export const cases = {
   "d-heart": dHeartCase,
   "demo": demoCase,
   "nathan": nathanCase,
+  "ita-airways": itaAirwaysCase,
 }
